06fcb491d24e1c63e8e7ab20d06e578dc0b9ae71
aryaganteng <hadi.argaya@gmail.com>
2023-01-25 14:39:12 +0700

    commit 2

---
+++ src/dasarreact/Variabel.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// const harga = 3000;
+
+// var harga = 2000;
+// if(true){
+//     var harga = 40000;
+// }
+
+let harga = 20000;
+if(true){
+    let harga = 40000;
+}
+
+
+const Variabel = () => {
+    return (
+        <div>
+            <h2>Harga : {harga}</h2>
+        </div>
+    )
+}
+
+export default Variabel
